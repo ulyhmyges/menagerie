53872f018fdd785063af3b15d777ad5471af3d7c
Ascensian <my.hugogerard@protonmail.com>
2023-05-11 00:18:17 +0200

    test lancement server ok

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("hello world!");
+import express from 'express';
+
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('my first animal'));
+
+app.listen(port, () => console.log(`application listening on port ${port}`));
+
